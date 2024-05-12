b642c22e9764bc9a2bb3fd495996df2ea3bae410
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2024-05-12 20:02:25 +0200

    First Practise

---
+++ practise/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './card/App.jsx'
+// import App from './props/App.jsx'
+// import App from './list/App.jsx'
+// import App from './eventHandler/App.jsx'
+// import App from './counter/App.jsx'
+// import App from './onChangePractise/App.jsx'
+// import App from './colorPicker/App.jsx'
+// import App from './objectUpdater/App.jsx'
+// import App from './arrayUpdater/App.jsx'
+// import App from './arrayOfObjectsUpdater/App.jsx'
+// import App from './toDoApp/App.jsx'
+import App from './XXXXXXXXXXXXXXXXXXX/App.jsx'
+// import App from './App.jsx'
+// import './card/index.css'
+// import './props/index.css'
+// import './list/index.css'
+// import './eventHandler/index.css'
+// import './counter/index.css'
+// import './onChangePractise/index.css'
+// import './colorPicker/index.css'
+// import './arrayUpdater/index.css'
+// import './arrayOfObjectsUpdater/index.css'
+// import './toDoApp/index.css'
+import './XXXXXXXXXXXXXXXXXX/index.css'
+// import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
